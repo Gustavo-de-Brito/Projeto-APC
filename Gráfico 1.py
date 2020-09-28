7135099fa15700b9fcc7572ae053e8d0b119ef59
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2020-09-28 17:24:26 -0300

    Fusão dos documentos
    Fusão dos documentos test em Dash.py e teste grafico.py, exclusão do documento grafico.html

--- Gráfico 1.py
+++ Gráfico 1.py
@@ -1,7 +1,14 @@
+#Importação do Dash
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+#Importação da biblioteca Plotly
 import plotly.offline as py
 import plotly.graph_objs as go
 py.init_notebook_mode(connected=True)
 
+#Leitura do documento .csv, separando as três fileiras do mesmo em três listas diferentes
 datas = []
 domesticas = []
 internacionais = []
@@ -13,6 +20,7 @@ for line in dados:
     domesticas.append(Do)
     internacionais.append(In)
 
+#Transformação dos dados no gráfico com os comandos do Plotly
 trace1 = go.Scatter(x = datas,
                    y = domesticas,
                    mode = 'lines',
@@ -23,6 +31,13 @@ trace2 = go.Scatter(x = datas,
                    mode = 'lines',
                    name = 'Internacionais',)
 
-data = [trace1, trace2]
-layout = go.Layout(title = 'Busca de Voos Domésticas vs. Internacionais',)
-py.plot(data, filename='grafico.html')
+gráfico_de_linhas = [trace1, trace2]
+
+#Importação do gráfico("gráfico_de_linhas") para exibição no Dash
+fig = go.Figure(gráfico_de_linhas)
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Graph(figure = fig)
+])
+
+app.run_server(debug = True, use_reloader = False)
